0a09b5289d6653fa150b248c02b8d2b450f03263
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-11-24 12:40:16 +0530

    REFACTORED: CRUD opearation and Documentation completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
-import {MatInputModule} from '@angular/material/input';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { HttpClientModule } from '@angular/common/http';
@@ -31,18 +31,18 @@ import { HttpClientModule } from '@angular/common/http';
     MatCheckboxModule,
     MatSliderModule,
     MatDatepickerModule,
-    MatButtonModule,FormsModule,ReactiveFormsModule,MatDialogModule,HttpClientModule
+    MatButtonModule, FormsModule, ReactiveFormsModule, MatDialogModule, HttpClientModule
   ],
   providers: [
-    { 
+    {
       provide: MatDialogRef,
       useValue: []
-       }, 
-      { 
-      provide: MAT_DIALOG_DATA, 
-      useValue: [] 
-      }
-],
+    },
+    {
+      provide: MAT_DIALOG_DATA,
+      useValue: []
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { HttpService } from 'src/app/services/http.service';
 import { AddEmployeeComponent } from '../add-employee/add-employee.component';
 
@@ -12,32 +12,49 @@ export class DashboardComponent implements OnInit {
   employeepayrollList: any;
   employeeCount: number = 10;
 
-  constructor(public dialog: MatDialog,  private httpuserservice: HttpService) { }
+  constructor(public dialog: MatDialog, private httpuserservice: HttpService) { }
 
+
+  /**
+   * All employee details is populated from the localstorage to the HOME page.
+   */
   ngOnInit(): void {
-    this.httpuserservice.getRequest('').sub
-  this.employeepayrollList =localStorage.getItem('EmployeePayrollList') ?
-  JSON.parse(localStorage.getItem('EmployeePayrollList')) : [] ;
-  this.employeeCount=this.employeepayrollList.length;
+    this.employeepayrollList = localStorage.getItem('EmployeePayrollList') ?
+      JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+    this.employeeCount = this.employeepayrollList.length;
 
   }
 
-remove(i: number) {
-  console.log(i);
-  console.log( this.employeepayrollList);
-  if (this.employeepayrollList!== null) { 
-    const empList = this.employeepayrollList;
-    empList.splice(i,1);
-    localStorage.setItem('EmployeePayrollList', JSON.stringify(empList));
-    this.employeeCount=this.employeepayrollList.length;
+  /**
+  * When the remove() is hit, the employee gets deleted from the localstorage and also the details is removed from the HOME page.
+  * Thus, a refreshed home page is rendered and a remove message is displayed to the user.
+  * 
+  * @param i remove() is invoked for a particular employee index.
+  */
+  remove(i: number) {
+    console.log(i);
+    console.log(this.employeepayrollList);
+    if (this.employeepayrollList !== null) {
+      const empList = this.employeepayrollList;
+      empList.splice(i, 1);
+      localStorage.setItem('EmployeePayrollList', JSON.stringify(empList));
+      this.employeeCount = this.employeepayrollList.length;
+    }
+    this.ngOnInit();
   }
-   this.ngOnInit();
-}
+
+
+  /**
+   * When the update() is hit, data of a particular index is populate in home page.
+   * The page gets navigated to the add page along with the employee object in the body.
+   * 
+   * @param i update() is invoked for a particular employee index.
+   */
   update(i: number) {
-    const dialogRef = this.dialog.open(AddEmployeeComponent,{
+    const dialogRef = this.dialog.open(AddEmployeeComponent, {
       data: i
     });
-    dialogRef.afterClosed().subscribe(result=>{
+    dialogRef.afterClosed().subscribe(result => {
       console.log(result);
       this.ngOnInit();
     })
